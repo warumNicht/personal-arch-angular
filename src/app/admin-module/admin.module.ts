3b05cf0d4bd06d847589421eefbd24a8794fc8a9
Andrey.Minev <andrey.minev@adesso.bg>
2020-08-21 10:52:10 +0300

    first grade

--- src/app/admin-module/admin.module.ts
+++ src/app/admin-module/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    AdminRoutingModule
+  ],
 })
 export class AdminModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from '../user/user.component';
-import { HomeComponent } from '../home/home.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin-module/admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'users', component: UserComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'admin', component: AdminComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
